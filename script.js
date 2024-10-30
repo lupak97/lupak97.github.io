33bee50906f354cea246e83d17c5e0c4d47694a2
lupak97 <lupakaplicacao@gmail.com>
2024-10-30 14:52:25 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+<button id="comprarBtn">Ir para Compras</button>
+
+<script>
+    document.getElementById('comprarBtn').onclick = function() {
+        window.location.href = 'compras.html'; // Altere para a URL correta
+    };
+</script>
