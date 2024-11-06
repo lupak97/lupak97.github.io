d60dc3802b7250dd115cc9841849e00328f076dc
lupak97 <lupakaplicacao@gmail.com>
2024-11-06 14:06:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 <button id="comprarBtn">Ir para Compras</button>
+$(document).ready(function() {
+    $('.carrossel').slick({
+        dots: true,                // Exibe os pontos de navegação abaixo do carrossel
+        infinite: true,            // O carrossel irá se mover infinitamente
+        speed: 500,                // Velocidade da transição (em milissegundos)
+        slidesToShow: 1,           // Exibe 1 imagem por vez
+        slidesToScroll: 1,         // Número de slides para rolar de cada vez
+        autoplay: true,            // O carrossel vai rodar automaticamente
+        autoplaySpeed: 2000,       // Intervalo entre cada slide (em milissegundos)
+        arrows: true,              // Exibe as setas de navegação
+        responsive: [
+            {
+                breakpoint: 768,   // Para dispositivos menores que 768px (tablets e celulares)
+                settings: {
+                    slidesToShow: 1,  // Mostra apenas 1 imagem
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
 
 <script>
     document.getElementById('comprarBtn').onclick = function() {
