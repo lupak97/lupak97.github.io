dd16c8a0fd9597fa7505f2ae08bccb42f251e674
lupak97 <lupakaplicacao@gmail.com>
2024-11-06 13:58:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,15 @@
         window.location.href = 'compras.html'; // Altere para a URL correta
     };
 </script>
+const mouseFollower = document.getElementById('mouse-follower');
+
+document.addEventListener('mousemove', (e) => {
+  mouseFollower.style.top = `${e.clientY - 20}px`; // Desloca o círculo
+  mouseFollower.style.left = `${e.clientX - 20}px`; // Desloca o círculo
+});
+document.addEventListener('mousemove', function(e) {
+  let x = e.clientX / window.innerWidth;
+  let y = e.clientY / window.innerHeight;
+  
+  document.body.style.backgroundPosition = `${x * 100}% ${y * 100}%`;
+});
